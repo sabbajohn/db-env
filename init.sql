8513410a62422fc7453dbd1bf4f33370776f3b39
John. Sabbá <45358247+sabbajohn@users.noreply.github.com>
2025-09-22 15:06:44 -0300

    Create root user and grant privileges

--- init.sql
+++ init.sql
@@ -5,12 +5,14 @@ CREATE DATABASE simples_web_laiz_db;
 CREATE DATABASE simples_web_carlos_db;
 CREATE DATABASE simples_web_nicolas_db;
 
+CREATE USER 'root'@'%' IDENTIFIED BY 'senha_root'
 CREATE USER 'john'@'%' IDENTIFIED BY 'senha_john';
 CREATE USER 'vini'@'%' IDENTIFIED BY 'senha_vini';
 CREATE USER 'laiz'@'%' IDENTIFIED BY 'senha_laiz';
 CREATE USER 'carlos'@'%' IDENTIFIED BY 'senha_carlos';
 CREATE USER 'nicolas'@'%' IDENTIFIED BY 'senha_nicolas';
 
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
 GRANT ALL PRIVILEGES ON simples_web_john_db.* TO 'john'@'%';
 GRANT ALL PRIVILEGES ON simples_web_vini_db.* TO 'vini'@'%';
 GRANT ALL PRIVILEGES ON simples_web_laiz_db.* TO 'laiz'@'%';
